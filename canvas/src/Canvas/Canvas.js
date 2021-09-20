fec18461f0efec3ac6f934d8a786a06c502da476
RuslanLaptev98 <ruslanlaptev98@gmail.com>
2021-09-21 00:36:54 +0300

    final commit

--- canvas/src/Canvas/Canvas.js
+++ canvas/src/Canvas/Canvas.js
@@ -1,84 +1,49 @@
 import React from 'react'
 import './Canvas.css'
 import rough from 'roughjs/bundled/rough.esm'
+import { chooseColor, createLine, combineLinesIntoRectangle } from './functions'
+import {
+    handleMouseUp,
+    handleMouseDown,
+    handleMouseMove,
+} from './drawingEventHandlers'
 
 const Canvas = () => {
-    // rough generator
     const generator = rough.generator()
 
-    // Ref & State
     const canvasRef = React.useRef(null)
+
     const [rectangles, setRectangles] = React.useState([])
     const [lines, setLines] = React.useState([])
     const [isDrawing, setIsDrawing] = React.useState(false)
+    const [color, setColor] = React.useState('#5bc197')
 
-    // Drawing event handlers
-    const handleMouseDown = (event) => {
-        setIsDrawing(true)
-
-        const position = canvasRef.current.getBoundingClientRect()
-        const { clientX, clientY } = event
-        const positionX = clientX - position.left
-        const positionY = clientY - position.top
-        const line = createLine(positionX, positionY, positionX, positionY)
-        setLines((prevState) => [...prevState, line])
-    }
-    const handleMouseMove = (event) => {
-        if (!isDrawing) return
-
-        const currentLineIndex = lines.length - 1
-        const { x1, y1 } = lines[currentLineIndex]
-        const { clientX, clientY } = event
-        const position = canvasRef.current.getBoundingClientRect()
-        const positionX = clientX - position.left
-        const positionY = clientY - position.top
-        const updatedLine = createLine(x1, y1, positionX, positionY)
-
-        const linesCopy = [...lines]
-        linesCopy[currentLineIndex] = updatedLine
-        setLines(linesCopy)
-    }
-    const handleMouseUp = () => {
-        setIsDrawing(false)
-    }
-
-    // Function for creating a line
-    const createLine = (x1, y1, x2, y2) => {
-        const roughLine = generator.line(x1, y1, x2, y2, { stroke: '#5bc197' })
-        return { x1, y1, x2, y2, roughLine }
-    }
-
-    // Drawing on rerender
     React.useEffect(() => {
+        // Setting canvas and context
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         context.clearRect(0, 0, canvas.width, canvas.height)
         const roughCanvas = rough.canvas(canvas)
 
+        // Drawing lines
         lines.forEach((line) => roughCanvas.draw(line.roughLine))
 
-        // Convert lines to rectangle
-        /*
-        roughCanvas.rectangle(100, 100, 100, 100, { stroke: '#5bc197' })
-        */
-        // среднее между четырьмя самыми высокими и среднее между четырьмя самыми низкими для Y
-        // среднее между четырьмя самыми левыми и среднее между четырьмя самыми правыми для X
-
-        // Convert lines to rectangle
-        if (lines.length && lines.length % 4 === 0 && isDrawing === false) {
-            console.log('4 lines')
-            const rectangle = generator.rectangle(100, 100, 100, 100, {
-                stroke: '#5bc197',
-            })
-            setRectangles((prevState) => [...prevState, rectangle])
-        }
+        // Combining 4 lines into rectangle
+        combineLinesIntoRectangle(
+            generator,
+            isDrawing,
+            lines,
+            color,
+            setLines,
+            setColor,
+            setRectangles,
+            chooseColor
+        )
     }, [lines, isDrawing])
 
     React.useEffect(() => {
-        const canvas = canvasRef.current
-        const roughCanvas = rough.canvas(canvas)
+        const roughCanvas = rough.canvas(canvasRef.current)
         rectangles.forEach((rectangle) => roughCanvas.draw(rectangle))
-        console.log(rectangles.length)
     })
 
     return (
@@ -88,9 +53,30 @@ const Canvas = () => {
                 ref={canvasRef}
                 width={window.innerWidth * 0.8}
                 height={window.innerHeight * 0.8}
-                onMouseDown={handleMouseDown}
-                onMouseMove={handleMouseMove}
-                onMouseUp={handleMouseUp}
+                onMouseDown={(event) =>
+                    handleMouseDown(
+                        event,
+                        generator,
+                        canvasRef,
+                        color,
+                        setIsDrawing,
+                        setLines,
+                        createLine
+                    )
+                }
+                onMouseMove={(event) =>
+                    handleMouseMove(
+                        event,
+                        generator,
+                        canvasRef,
+                        color,
+                        isDrawing,
+                        lines,
+                        setLines,
+                        createLine
+                    )
+                }
+                onMouseUp={() => handleMouseUp(setIsDrawing)}
             ></canvas>
         </div>
     )
